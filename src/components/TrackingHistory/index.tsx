aab7bfb98ec0258f25fe3056665af6e3f6296370
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2024-12-30 14:46:00 -0300

    fix: endTime in tracking_history

--- src/components/TrackingHistory/index.tsx
+++ src/components/TrackingHistory/index.tsx
@@ -4,7 +4,7 @@ import {
   useTrackingProvider,
 } from "../../contexts/Tracking";
 import { BsPlay, BsStop } from "solid-icons/bs";
-import { createSignal, For, Show } from "solid-js";
+import { createEffect, createSignal, For, Show } from "solid-js";
 import { RiArrowsArrowDownSFill } from "solid-icons/ri";
 interface DTO extends Omit<TrackingHistoryProps, "timer"> {
   tracking?: boolean;
@@ -13,8 +13,14 @@ interface DTO extends Omit<TrackingHistoryProps, "timer"> {
 const TrackingHistory = (props: DTO) => {
   const { startTracking, stopTracking } = useTrackingProvider();
   const startDateFormated = format(props.startTime, "HH:mm");
-  const endDateFormated = format(props.endTime, "HH:mm");
+  const [endDateFormated, setEndDateFormat] = createSignal(
+    format(props.endTime, "HH:mm"),
+  );
   const [showHistory, setShowHistory] = createSignal(false);
+
+  createEffect(() => {
+    setEndDateFormat(format(props.endTime, "HH:mm"));
+  });
 
   return (
     <div>
@@ -24,7 +30,7 @@ const TrackingHistory = (props: DTO) => {
         <p class="flex-1 text-lg text-primary">{props.description}</p>
         <div class="flex">
           <p class="border-dashed px-2 border-black border-l- border-r">
-            {startDateFormated}-{endDateFormated}
+            {startDateFormated}-{endDateFormated()}
           </p>
           <p class="border-dashed px-2 border-black border-r">
             {props.formattedTime}
@@ -53,8 +59,13 @@ const TrackingHistory = (props: DTO) => {
       <Show when={showHistory()}>
         <For each={props.tracking_history}>
           {(history, index) => {
-            const startDateFormated = format(props.startTime, "HH:mm");
-            const endDateFormated = format(props.endTime, "HH:mm");
+            const startDateFormated = format(history.startTime, "HH:mm");
+            const [endDateFormated, setEndDateFormat] = createSignal(
+              format(history.endTime, "HH:mm"),
+            );
+            createEffect(() => {
+              setEndDateFormat(format(history.endTime, "HH:mm"));
+            });
             return (
               <div
                 class={`bg-slate-100 ${index() + 1 === props.tracking_history.length ? "rounded-lg" : ""} h-14 flex  shadow-md  py-3 px-4`}
@@ -62,7 +73,7 @@ const TrackingHistory = (props: DTO) => {
                 <p class="flex-1 text-lg text-primary">{history.description}</p>
                 <div class="flex">
                   <p class="border-dashed px-2 border-black border-l- border-r">
-                    {startDateFormated}-{endDateFormated}
+                    {startDateFormated}-{endDateFormated()}
                   </p>
                   <p class="border-dashed px-2 border-black border-r">
                     {history.formattedTime}
